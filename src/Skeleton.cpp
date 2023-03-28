bd8e6ab19acff6ed48b546ba0bad11a94d55d4f9
Kavefozogepezet <apostagibalint@gmail.com>
2023-03-28 13:48:07 +0200

    Added trail, game goal, and more math

--- src/Skeleton.cpp
+++ src/Skeleton.cpp
@@ -34,6 +34,9 @@
 #include "framework.h"
 
 #include <iostream>
+#include <cmath>
+#include <random>
+#include <time.h>
 
 namespace Color {
 	const vec3
@@ -75,11 +78,16 @@ public:
 	* Draws a line segment on the hyperbolic surface using the Beltrami-Poincare projection.
 	* @param p1, p2 the two ends of the line segment
 	* @param color the color of the line segment
-	* @param t the thickness of the line
 	* @res the number of points to approximate the line segment
 	*/
 	void drawLine(const vec3& p1, const vec3& p2, const vec3& color, size_t res = 16);
 
+	/*
+	* Draws line segments on the hyperbolic surface using the Beltrami-Poincare projection.
+	* @param points the points of the line segments
+	* @param color the color of the line segment
+	* @res the number of points to approximate the line segment
+	*/
 	void drawLineStrip(const std::vector<vec3>& points, const vec3& color);
 private:
 	unsigned int vao, vbo;
@@ -99,21 +107,37 @@ namespace HypMath {
 	float lorentz(const vec3& v1, const vec3& v2);
 
 	/*
-	* Interpolates two points on thr hyperbolic surface
-	* @param p1, p2 the two points
-	* @param a the ratio between the two points
+	* @param v1 A vector
+	* @returns the length of the vector
 	*/
-	vec3 interpolate(const vec3& p1, const vec3& p2, float a);
+	float length(const vec3& v);
+
+	/*
+	* Calculates the distance, and the direction of two points.
+	* @param p, q the two points
+	* @param v the function will set it to the direction of the line from p to q
+	* @param d the function will set it to the distance between p and q
+	*/
+	void distance(const vec3& p, const vec3& q, vec3& v, float& d);
+
+	/*
+	* Moves a point in a direction with the given distance
+	* @param p the point to move, its value is changed to the new point
+	* @param d the direction to move the point in
+	* @param l the distance between the old and new points
+	*/
+	void move(vec3& p, const vec3& d, float l);
 
 	/*
 	* Walks at a given direction from the starting point.
 	* If the velocity is a unit vector, the distance of the end point
 	* from the starting point will be dt.
 	* @param p the starting point of the walk, its value is changed to the end point of the walk
-	* @param v the velocity wich is a valid vector at p, its value is changed th the new velocity at he endpoint
+	* @param d the direction wich is a valid vector at p, its value is changed th the new velocity at he endpoint 
+	* @param v the walking speed
 	* @param dt the duration of the walk
 	*/
-	void walk(vec3& p, vec3& v, float dt);
+	void walk(vec3& p, vec3& d, float v, float dt);
 
 	/*
 	* Shifths the position and direction, as if it was on a circular orbit.
@@ -125,7 +149,12 @@ namespace HypMath {
 	*/
 	void orbit(vec3& p, vec3& d, float w, float v, float dt);
 
-	// TODO comment
+	/*
+	* Calculates the vector, that is rotated 90 degrees.
+	* @param v the vector to be rotated
+	* @param p the point, where v is a valid vector
+	* @return the perpendicular vector
+	*/
 	vec3 perpendicularAt(const vec3& v, const vec3& p);
 
 	/*
@@ -147,62 +176,100 @@ namespace HypMath {
 	void correct(vec3& p, vec3& v);
 }
 
+/*
+* Specifies the arc of the motion of a UFOHami
+* LEFT, RIGHT: rotating 
+* LEFT_FORWARD, RIGHT_FORWARD: circular path 
+* FORWARD: straight path
+*/
 enum class ArcType {
 	LEFT,
 	LEFT_FORWARD,
 	FORWARD,
 	RIGHT_FORWARD,
-	RIGHT
+	RIGHT,
+	UNKNOWN
 };
 
 class UFOHami {
 public:
-	UFOHami(vec3 color) : color(color) {}
+	UFOHami(vec3 color) : color(color) {
+		if (!initrand) {
+			srand(time(NULL));
+			initrand = true;
+		}
 
+		float
+			randAngle = (float(rand() % 6283) * 0.01f),
+			randDirAngle = (float(rand() % 6283) * 0.01f),
+			randDistance = float(rand() % (SPAWN_RADIUS * 100)) * 0.01f;
+		direction = HypMath::rotateAt(direction, randAngle, position);
+		HypMath::walk(position, direction, 1.0f, randDistance);
+		direction = HypMath::rotateAt(direction, randDirAngle, position);
+		HypMath::correct(position, direction);
+
+		lastPos = position;
+		lastDir = direction;
+	}
+
+	/*
+	* Sets where the UFOHami will look.
+	* @param otherHami a pointer to a hami, that this hami will follow with its eyes.
+	*/
+	void lookingAt(UFOHami* otherHami) {
+		eyeTarget = otherHami;
+	}
+
+	/*
+	* @returns whether the hami eat the other hami or not
+	*/
+	bool eatHami(UFOHami& otherHami) {
+		vec3 temp;
+		float distance;
+		HypMath::distance(position, otherHami.position, temp, distance);
+		return distance < RADIUS * 2.0f;
+	}
+
+	/* Set wether the UFOHami is moving, or is staying still. */
 	void setMoving(bool value) { moving = value;  }
 
+	/* Set the arc of the movement the movement */
 	void setMovement(ArcType value) {
 		if (movement == value)
 			return;
-
-		// concatenate trail with current arc
-		trail.reserve(currentArc.size());
-		trail.insert(trail.end(), currentArc.begin(), currentArc.end());
-		currentArc.clear();
-		
-		// prepare movement for the next arc
-		lastPos = position;
-		lastDir = direction;
-		delta = 0.0f;
-
+		resetArc();
 		movement = value;
-		std::cout << "arc changed" << std::endl;
 	}
 
+	/*
+	* The eyes and mouth of the UFOHami is animated.
+	* If moving, the UFOHami moves forward on the arc of its movement.
+	* @param dt the delta time that passed since the last call of this function
+	*/
 	void animate(float dt) {
-		animation += dt * 5.0f;
+		animation += dt * ANIMATION_SPEED;
 
 		if (moving) {
 			delta += dt;
+			if (delta > MAXIMUM_ARC_LENGTH)
+				resetArc();
 
 			position = lastPos;
 			direction = lastDir;
 
+			float omega = OMEGA;
+
 			switch (movement) {
 			case ArcType::FORWARD:
-				HypMath::walk(position, direction, delta * VELOCITY);
+				HypMath::walk(position, direction, VELOCITY, delta);
 				break;
+			case ArcType::RIGHT_FORWARD: omega = -OMEGA;
 			case ArcType::LEFT_FORWARD:
-				HypMath::orbit(position, direction, OMEGA, VELOCITY, delta);
+				HypMath::orbit(position, direction, omega, VELOCITY, delta);
 				break;
-			case ArcType::RIGHT_FORWARD:
-				HypMath::orbit(position, direction, -OMEGA, VELOCITY, delta);
-				break;
+			case ArcType::RIGHT: omega = -OMEGA;
 			case ArcType::LEFT:
-				direction = HypMath::rotateAt(lastDir, delta * OMEGA * M_PI * 2, position);
-				break;
-			case ArcType::RIGHT:
-				direction = HypMath::rotateAt(lastDir, -delta * OMEGA * M_PI * 2, position);
+				direction = HypMath::rotateAt(lastDir, delta * omega * M_PI * 2, position);
 				break;
 			}
 			HypMath::correct(position, direction);
@@ -211,27 +278,39 @@ public:
 		}
 	}
 
+	/* Draws the UFOHami without its trail */
 	void draw() {
 		Renderer.drawCircle(position, RADIUS, color, 32);
-		vec3 mouthPos = position, mouthVec = direction;
-		HypMath::walk(mouthPos, mouthVec, RADIUS * 0.9f);
+
+		vec3 mouthPos = position;
+		HypMath::move(mouthPos, direction, RADIUS * 0.9f);
 		Renderer.drawCircle(mouthPos, MOUTH_RADIUS * sinf(animation), Color::black);
+
 		const float eyeRots[2] = { EYE_ANGLE, -EYE_ANGLE };
 		for (auto eyeRot : eyeRots) {
 			vec3 eyePos = position;
 			vec3 eyeVec = HypMath::rotateAt(direction, eyeRot, position);
-			HypMath::walk(eyePos, eyeVec, RADIUS);
+			HypMath::move(eyePos, eyeVec, RADIUS);
 			Renderer.drawCircle(eyePos, EYE_RADIUS, Color::white);
-			Renderer.drawCircle(eyePos, EYE_RADIUS / 2.0f, Color::black);
+
+			if (eyeTarget) {
+				vec3 pupilDir;
+				float temp;
+				HypMath::distance(eyePos, eyeTarget->position, pupilDir, temp);
+				HypMath::move(eyePos, pupilDir, EYE_RADIUS * 0.5f);
+			}
+			Renderer.drawCircle(eyePos, EYE_RADIUS * 0.5f, Color::black);
 		}
 	}
 
+	/* Draws thee trail of the UFOHami. */
 	void drawTrail() {
 		Renderer.drawLineStrip(trail, Color::white);
+		setPointsOfCurrentArc();
+		Renderer.drawLineStrip(currentArc, Color::white);
 	}
-private:
 	vec3 position{ 0, 0, 1 }, direction{ 1, 0, 0 }, color;
-
+private:
 	ArcType movement = ArcType::FORWARD;
 	vec3 lastPos = position, lastDir = direction;
 	float delta = 0.0f;
@@ -242,33 +321,103 @@ private:
 
 	float animation = 0.0f;
 
+	UFOHami* eyeTarget = nullptr;
+
+	/* recalculates the trail points of the current arc. */
+	void setPointsOfCurrentArc() {
+		float omega = OMEGA;
+		float distance = delta * VELOCITY;
+		size_t res = size_t(distance / TRAIL_RESOLUTION) + 1;
+
+		currentArc.clear();
+		currentArc.reserve(res);
+
+		switch (movement) {
+		case ArcType::FORWARD: {
+			for (size_t i = 0; i <= res; i++) {
+				vec3
+					dir = lastDir,
+					trailPoint = lastPos;
+				float d = (distance * i) / float(res);
+				HypMath::move(trailPoint, dir, d);
+				currentArc.push_back(trailPoint);
+			}
+		} break;
+		case ArcType::RIGHT_FORWARD: omega = -OMEGA;
+		case ArcType::LEFT_FORWARD: {
+			for (size_t i = 0; i <= res; i++) {
+				vec3
+					dir = lastDir,
+					trailPoint = lastPos;
+				float d = (delta * i) / float(res);
+				HypMath::orbit(trailPoint, dir, omega, VELOCITY, d);
+				currentArc.push_back(trailPoint);
+			}
+		} break;
+		}
+	}
+
+	/*
+	* Updates the starting position and direction of the
+	* current arc to the current position and direction.
+	* Appends the trail with the points of the current arc.
+	*/
+	void resetArc() {
+		// concatenate trail with current arc
+		trail.reserve(currentArc.size());
+		trail.insert(trail.end(), currentArc.begin(), currentArc.end());
+		currentArc.clear();
+
+		// prepare movement for the next arc
+		lastPos = position;
+		lastDir = direction;
+		delta = 0.0f;
+	}
+private:
+	static bool initrand;
+
 	static constexpr float
 		RADIUS = 0.2f,
 		EYE_ANGLE = M_PI * 0.25f,
 		EYE_RADIUS = 0.075f,
 		MOUTH_RADIUS = 0.075f,
+		TRAIL_RESOLUTION = 0.2f,
+		ANIMATION_SPEED = 3.0f,
+		MAXIMUM_ARC_LENGTH = 10.0f,
 		
 		VELOCITY = 0.75f,
 		OMEGA = 0.5f;
-};
-/*
-vec3 point = { 0.0f, 0.0f, 1.0f };
-vec3 dir = { 1.0f, 0.0f, 0.0f };
-*/
 
-UFOHami
-	redhami { Color::red },
-	greenhami{ Color::green };
+	static constexpr int
+		SPAWN_RADIUS = 2;
+}
+redhami{ Color::red },
+greenhami{ Color::green };
 
+bool UFOHami::initrand = false;
+
+/* Contains the value of GLUT_ELAPSED_TIME at the last frame */
 long lastTime = 0;
 
-bool in_e = false, in_s = false, in_f = false;
+/* true if redhami eat greenhami. If true, all animation must stop. */
+bool gameOver = false;
+
+/* Contains the state of the keys. Each bool field represents the corresponding key. */
+struct Input {
+	bool e, s, f;
+
+	/* Returns the correct arc type based on the currents state of the keys. */
+	ArcType getArcType() const;
+} in;
 
 // Initialization, create an OpenGL context
 void onInitialization() {
 	Renderer.init();
 	greenhami.setMoving(true);
 	greenhami.setMovement(ArcType::RIGHT_FORWARD);
+
+	redhami.lookingAt(&greenhami);
+	greenhami.lookingAt(&redhami);
 }
 
 // Window has become invalid: Redraw
@@ -287,10 +436,13 @@ void onDisplay() {
 // Key of ASCII code pressed
 void onKeyboard(unsigned char key, int pX, int pY) {
 	switch (key) {
-	case 'e': in_e = true; break;
-	case 's': in_s = true; break;
-	case 'f': in_f = true; break;
+	case 'e': in.e = true; break;
+	case 's': in.s = true; break;
+	case 'f': in.f = true; break;
 	case 'p':
+		vec3 dirrot = HypMath::rotateAt(redhami.direction, 0.1f, redhami.position);
+		float angle = acos(HypMath::lorentz(dirrot, redhami.direction) / (HypMath::length(dirrot) * HypMath::length(redhami.direction)));
+		//std::cout << "angle: " << angle << std::endl;
 		break;
 	}
 }
@@ -298,41 +450,23 @@ void onKeyboard(unsigned char key, int pX, int pY) {
 // Key of ASCII code released
 void onKeyboardUp(unsigned char key, int pX, int pY) {
 	switch (key) {
-	case 'e': in_e = false; break;
-	case 's': in_s = false; break;
-	case 'f': in_f = false; break;
+	case 'e': in.e = false; break;
+	case 's': in.s = false; break;
+	case 'f': in.f = false; break;
 	}
 }
 
 // Move mouse with key pressed
-void onMouseMotion(int pX, int pY) {	// pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-	printf("Mouse moved to (%3.2f, %3.2f)\n", cX, cY);
-}
+void onMouseMotion(int pX, int pY) {}
 
 // Mouse click event
-void onMouse(int button, int state, int pX, int pY) { // pX, pY are the pixel coordinates of the cursor in the coordinate system of the operation system
-	// Convert to normalized device space
-	float cX = 2.0f * pX / windowWidth - 1;	// flip y axis
-	float cY = 1.0f - 2.0f * pY / windowHeight;
-
-	const char * buttonStat = "<invalid state>";
-	switch (state) {
-	case GLUT_DOWN: buttonStat = "pressed"; break;
-	case GLUT_UP:   buttonStat = "released"; break;
-	}
-
-	switch (button) {
-	case GLUT_LEFT_BUTTON:   printf("Left button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);   break;
-	case GLUT_MIDDLE_BUTTON: printf("Middle button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY); break;
-	case GLUT_RIGHT_BUTTON:  printf("Right button %s at (%3.2f, %3.2f)\n", buttonStat, cX, cY);  break;
-	}
-}
+void onMouse(int button, int state, int pX, int pY) {}
 
 // Idle event indicating that some time elapsed: do animation here
 void onIdle() {
+	if (gameOver)
+		return;
+
 	if (!time) {
 		lastTime = glutGet(GLUT_ELAPSED_TIME);
 		return;
@@ -342,39 +476,18 @@ void onIdle() {
 	if (time - lastTime < 10)
 		return;
 
+	if (redhami.eatHami(greenhami))
+		gameOver = true;
+
 	float dt = float(time - lastTime) / 1000.0f;
+	auto arc = in.getArcType();
 
-	bool in_rotate = in_s != in_f;
-	redhami.setMoving(true); // assume movement
-
-	if (in_e) {
-		if (in_rotate) {
-			if (in_s)
-				redhami.setMovement(ArcType::LEFT_FORWARD);
-			else
-				redhami.setMovement(ArcType::RIGHT_FORWARD);
-		}
-		else {
-			redhami.setMovement(ArcType::FORWARD);
-		}
+	redhami.setMoving(false); // assume the hami stopped moving
+	if (arc != ArcType::UNKNOWN) {
+		redhami.setMovement(arc);
+		redhami.setMoving(true);
 	}
-	else if(in_rotate) {
-		if (in_s)
-			redhami.setMovement(ArcType::LEFT);
-		else
-			redhami.setMovement(ArcType::RIGHT); 
-	}
-	else {
-		redhami.setMoving(false); // assumption was incorrect
-	}
-
-	//std::cout << dt << std::endl;
-
-	/*
-	HypMath::walk(point, dir, dt * input.x / 5.0f);
-	dir = HypMath::rotateAt(dir, (input.y - input.z) * dt, point);
-	HypMath::correct(point, dir);
-	*/
+	
 	redhami.animate(dt);
 	greenhami.animate(dt);
 
@@ -499,15 +612,14 @@ void HypRenderer::drawCircle(const vec3& c, float r, const vec3& color, size_t r
 
 	vec3 r1 = { 1, 0, 0 }, r2 = { 0, 1, 0 };
 
-	vec3 rdir = rv;
 	vec3 last = c;
-	HypMath::walk(last, rdir, r);
+	HypMath::move(last, rv, r);
 
 	for (size_t i = 0; i < res; i++) {
 		angle += increment;
-		rdir = HypMath::rotateAt(rv, angle, c);
+		vec3 rdir = HypMath::rotateAt(rv, angle, c);
 		vec3 current = c;
-		HypMath::walk(current, rdir, r);
+		HypMath::move(current, rdir, r);
 		vertices.push_back(c);
 		vertices.push_back(last);
 		vertices.push_back(current);
@@ -542,6 +654,15 @@ float HypMath::lorentz(const vec3& v1, const vec3& v2) {
 	return v1.x * v2.x + v1.y * v2.y - v1.z * v2.z;
 }
 
+float HypMath::length(const vec3& v) {
+	return sqrtf(lorentz(v, v));
+}
+
+void HypMath::distance(const vec3& p, const vec3& q, vec3& v, float& d) {
+	d = acoshf(-lorentz(q, p));
+	v = (q - p * coshf(d)) / sinhf(d);
+}
+
 vec3 HypMath::perpendicularAt(const vec3& v, const vec3& p) {
 	vec3 vp;
 	float expr1 = v.z * p.x - p.z * v.x;
@@ -555,7 +676,7 @@ vec3 HypMath::perpendicularAt(const vec3& v, const vec3& p) {
 			z = (p.x * x + p.y) / p.z;
 		vp = { x, 1.0f, z };
 	}
-	vp = vp / sqrtf(lorentz(vp, vp));
+	vp = vp / HypMath::length(vp);
 	// correct direction
 	if (cross(v, vp).z < 0.0f)
 		vp = -vp;
@@ -567,9 +688,15 @@ vec3 HypMath::rotateAt(const vec3& v, float a, const vec3& p) {
 	return cosf(a) * v + sinf(a) * vp;
 }
 
-void HypMath::walk(vec3& p, vec3& v, float dt) {
-	vec3 p2 = p * coshf(dt) + v * sinhf(dt);
-	v = p * sinhf(dt) + v * coshf(dt);
+void HypMath::move(vec3& p, const vec3& d, float l) {
+	p = p * coshf(l) + d * sinhf(l);
+}
+
+void HypMath::walk(vec3& p, vec3& d, float v, float dt) {
+	float l = v * dt;
+	vec3 p2 = p;
+	move(p2, d, l);
+	d = p * sinhf(l) + d * coshf(l);
 	p = p2;
 }
 
@@ -578,7 +705,7 @@ void HypMath::correct(vec3& p, vec3& v) {
 	vec3 n = normalize({ p.x, p.y, -p.z });
 	float e = dot(v, n);
 	v = v - n * e;
-	v = v / sqrtf(lorentz(v, v));
+	v = v / HypMath::length(v);
 }
 
 void HypMath::orbit(vec3& p, vec3& d, float w, float v, float dt) {
@@ -588,11 +715,29 @@ void HypMath::orbit(vec3& p, vec3& d, float w, float v, float dt) {
 	vec3 radius = perpendicularAt(d, p);
 
 	// calculation the centre and radius of the orbit
-	walk(centre, radius, r);
+	walk(centre, radius, 1.0f, r);
 	radius = HypMath::rotateAt(-radius, w * dt, centre);
 
 	// calculating end point of the orbit
-	walk(centre, radius, r);
+	walk(centre, radius, 1.0f, r);
 	p = centre;
 	d = perpendicularAt(radius, p);
+}
+
+ArcType Input::getArcType() const {
+	bool in_rotate = s != f;
+	if (e)
+		if (in_rotate)
+			if (s)
+				return ArcType::LEFT_FORWARD;
+			else
+				return ArcType::RIGHT_FORWARD;
+		else
+			return ArcType::FORWARD;
+	else if (in_rotate)
+		if (s)
+			return ArcType::LEFT;
+		else
+			return ArcType::RIGHT;
+	return ArcType::UNKNOWN;
 }
